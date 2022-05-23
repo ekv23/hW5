a85af018ac276d4d5ef387a1dd1bd305cc1e2847
Ekaterina <ekaterina.s.velichko@gmail.com>
2022-05-23 20:30:10 +0300

    task1

---
+++ task1/Program.cs
@@ -0,0 +1,26 @@
+﻿//Задача 1: Задайте массив заполненный случайными положительными 
+//трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+
+//[345, 897, 568, 234] -> 2
+
+void FillNewArray(int[] Array)
+{
+ Console.Write("Случайный массив: ");
+ for (int i = 0; i < Array.Length; i++)
+ {
+ Array[i] = new Random().Next(100, 999);
+ Console.Write($"{Array[i]} ");
+ } 
+ Console.WriteLine();
+ int count = 0;
+ for (int x = 0; x < Array.Length; x++)
+ {
+ if(Array[x]%2 == 0) count++; 
+ }
+ Console.WriteLine($"Количество четных чисел в массиве: {count}");
+}
+Console.WriteLine("введите количество чисел в массиве: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[N];
+FillNewArray(array);
+Console.WriteLine();
